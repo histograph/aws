2a5de09fed50b47ecd87a66da50bb1e5fb5e5f5e
Jelle Herold <jelle@defekt.nl>
2015-09-28 18:12:11 +0200

    some rediculous logging function.. oh well

--- log.py
+++ log.py
@@ -1,4 +1,20 @@
 from datetime import datetime
+from sys import stdout
+
+# ZOMG .__/`\,-^~.__/`\,-^~.__/`\,-^~.__/`\,-^~
+c = 0
+progress = ".__/`\,-^~"
+def whee():
+    global c
+    c += 1
+    return progress[c % len(progress)]
+
+# ok I'm calm again...
 
 def log(msg):
-    print("[%s] %s" % (datetime.now().isoformat(), msg))
+    # log single "dot"
+    if(len(msg) == 1):
+        stdout.write(whee())
+        stdout.flush()
+    else:
+        print("[%s] %s" % (datetime.now().isoformat(), msg))
