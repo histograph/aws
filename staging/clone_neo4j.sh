76493f6548ff925ef351e702ebf41287bf383cbb
Stefano Bocconi <stefano@waag.org>
2016-03-15 17:15:58 +0100

    reading parameters from command line or yaml file

--- staging/clone_neo4j.sh
+++ staging/clone_neo4j.sh
@@ -2,4 +2,11 @@
 
 #create AMI from neo4j production instance, waits until AMI creation is done
 #and launch instance based on neo4j AMI, tags name, waits until status ok
-python scripts/create_image.py
+
+if (( $# != 1 ))
+then
+	echo "illegal number of parameters, expected 1, got $#"
+	exit 1
+fi
+
+python scripts/create_image.py ${1}
